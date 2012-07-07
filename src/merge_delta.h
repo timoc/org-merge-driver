d499fc454f821a7465513d9d21e6cae95c1cab95
Andrew Young <youngar17@gmail.com>
2012-07-07 19:50:43 -0400

    merge_delta: update gets and sets, move changes
    merge delta was missing a few of the gets and sets for changing the
    document element it pointed to.
    
    Change types were moved into there own header since many things use
    different change types.

--- src/merge_delta.h
+++ src/merge_delta.h
@@ -6,30 +6,20 @@
 #define MERGE_DELTA_H
 
 #include <stdlib.h>
+#include "merge_map.h"
+#include "merge_change.h"
 
 struct merge_map;
 typedef struct merge_map merge_map;
 
-typedef enum merge_delta_type 
-  {
-    merge_no_change,
-    merge_update,
-    merge_delete,
-    merge_add,
-    merge_move
-  } merge_delta_type;
-
-typedef enum merge_delta_src
-  {
-    merge_delta_local_src,
-    merge_delta_remote_src,
-    mergedelta_ancestor_src
-  } merge_delta_src;
+struct doc_elt;
+typedef struct doc_elt doc_elt;
 
 typedef struct merge_delta
 {
-  merge_delta_type type;
-  merge_delta_src src;
+  doc_src src;
+  doc_elt *elt;
+  merge_change type;
   merge_map *map;
 } merge_delta;
 
@@ -45,41 +35,6 @@ merge_delta_free (merge_delta *delta)
   free (delta);
 }
 
-static inline merge_delta_type
-merge_delta_get_type (merge_delta *delta)
-{
-  return delta->type;
-}
-
-static inline void
-merge_delta_set_type (merge_delta *delta, merge_delta_type type)
-{
-  delta->type = type;
-  return;
-}
-
-static inline merge_delta_src
-merge_delta_get_src (merge_delta *delta)
-{
-  return delta->src;
-}
-
-static inline void
-merge_delta_set_src (merge_delta *delta, merge_delta_src src)
-{
-  delta->src = src;
-  return;
-}
-
-static inline merge_delta *
-merge_delta_create (merge_delta_type type, merge_delta_src src)
-{
-  merge_delta *d = merge_delta_create_empty();
-  merge_delta_set_type (d, type);
-  merge_delta_set_src (d, src);
-  return d;
-}
-
 static inline merge_map *
 merge_delta_get_map (merge_delta *delta)
 {
@@ -87,10 +42,16 @@ merge_delta_get_map (merge_delta *delta)
 }
 
 static inline void
-merge_delta_set_map (merge_delta *delta, merge_map *map)
+merge_delta_set_map (merge_delta * delta, merge_map *map)
 {
   delta->map = map;
   return;
+}
+
+static inline doc_elt *
+merge_delta_get_elt (merge_delta *delta)
+{
+  return delta->elt;
 }
 
 #endif
