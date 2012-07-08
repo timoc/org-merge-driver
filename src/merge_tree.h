e5e27352a6ae7eb36ed146a6a482433af4e5c2b0
Andrew Young <youngar17@gmail.com>
2012-07-08 04:07:27 -0400

    Add merge_tree helper functions

--- src/merge_tree.h
+++ src/merge_tree.h
@@ -1,10 +1,58 @@
+/**
+ * @file merge_tree.h 
+ *
+ * A wrapper to ltree, used to represent a merge tree.  A merge tree
+ * is an ltree that stores only merge_delta as data.
+ */
+
 #ifndef MERGE_TREE_H
 #define MERGE_TREE_H
 
 #include "ltree.h"
 
+struct merge_delta;
+typedef struct merge_delta merge_delta;
+
 typedef ltree      merge_tree;
 typedef ltree_node merge_node;
 typedef ltree_list merge_list;
+
+static inline merge_delta *
+merge_node_get_delta (merge_node *node)
+{
+  return (merge_delta *)(ltree_node_get_data ((ltree_node *)node));
+}
+
+static inline void
+merge_node_set_delta (merge_node *node, merge_delta * delta)
+{
+  ltree_node_set_data ((ltree_node *)node, (void *)delta);
+}
+
+static inline merge_node *
+merge_node_get_parent (merge_node *node)
+{
+  return (merge_node *)(ltree_node_get_parent ((ltree_node *)node));
+}
+
+static inline void
+merge_node_set_parent (merge_node *node, merge_node *parent)
+{
+  ltree_node_set_parent ((ltree_node *)node, (ltree_node *)parent);
+  return;
+}
+
+static inline merge_list
+merge_node_get_children (merge_node *node)
+{
+  return (merge_list)ltree_node_get_children ((ltree_node *)node);
+}
+
+static inline void
+merge_node_set_children (merge_node *node, merge_list children)
+{
+  ltree_node_set_children ((ltree_node *)node, (ltree_list)children);
+  return;
+}
 
 #endif
