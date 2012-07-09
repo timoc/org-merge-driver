c6db1150676cd385aafc82f5dbbd63dc5466a5b2
Andrew Young <youngar17@gmail.com>
2012-07-09 11:13:33 -0400

    Add implementation for merge_print_ctxt

---
+++ src/merge_print_ctxt.h
@@ -0,0 +1,16 @@
+/**
+ * @file merge_print_cxt.h
+ */
+
+#ifndef MERGE_PRINT_CTXT_H
+#define MERGE_PRINT_CTXT_H
+
+/**
+ * A context for merge_print.
+ */
+typedef struct merge_print_ctxt
+{
+  int depth; /**< depth, used for testing */
+} merge_print_ctxt;
+
+#endif
