8b313455c9418463a7f1c833b9db58972847f90f
Andrew Young <youngar17@gmail.com>
2012-07-12 00:35:10 -0400

    Add debug print levels

--- src/debug.h
+++ src/debug.h
@@ -11,6 +11,13 @@
 #define DEBUG
 #endif /* DEBUG */
 
+#define DEFAULT_PRINTLEVEL 3
+#define MAIN_PRINTLEVEL DEFAULT_PRINTLEVEL
+#define MERGE_PRINTLVEL DEFAULT_PRINTLEVEL
+#define DOC_PRINTLEVEL DEFAULT_PRINTLEVEL
+#define LEXER_PRINTLEVEL DEFAULT_PRINTLEVEL
+#define PARSER_PRINTLEVEL DEFAULT_PRINTLEVEL
+
 /* DEBUG_ON is used to test if debug is enabled */
 #ifdef DEBUG
 #define DEBUG_ON 1
@@ -18,16 +25,9 @@
 #define DEBUG_ON 0
 #endif /* USE_DEBUG */
 
-/* Utility Macros */
-#define XCONCAT(a,b) a ## b
-#define CONCAT(a,b) XCONCAT(a,b)
-#define XQUOTE(str) #str
-#define QUOTE(str) XQUOTE(str)
-#define EVAL(val) val
-
 #ifndef DEBUG_OUT 
 #define DEBUG_OUT stderr
-#endif /* DEBUG_OUT */
+#endif
 
 #ifndef DEBUG_PRINT
 #define DEBUG_PRINT 1
@@ -38,15 +38,13 @@
 #define DEBUG_MODULE DEFAULT
 #endif /* DEBUG_MODULE */
 
-#define DEFAULT_DEBUG_ON 0
-#define MAIN_DEBUG_ON 1
+#define debug_printf(format, ...) \
+  debug_msg(DEFAULT, DEFAULT_PRINTLEVEL, format, ##__VA_ARGS__)
 
-#define debug_printf(format, ...)				\
-  do { if (DEBUG_ON && CONCAT(DEBUG_MODULE,_DEBUG_ON)) {	\
-      fprintf (DEBUG_OUT,					\
-	       QUOTE(DEBUG_MODULE) ":%s:%s:%d: " format,	\
-	       __FILE__, __func__, __LINE__,			\
-	       ##__VA_ARGS__); } } while (0)
-  
+#define debug_msg(module, level, format, ...)                   \
+  do { if (DEBUG_ON && (level <= module ## _PRINTLEVEL)) {      \
+    fprintf (DEBUG_OUT, #module"-%d:%s:%d:%s " format,          \
+      level, __FILE__, __LINE__, __func__, ##__VA_ARGS__);	\
+    }} while (0)
 
 #endif /*DEBUG_H */
