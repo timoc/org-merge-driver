cb97a82527180952c35840df321cfc3bca6feef8
Andrew Young <youngar17@gmail.com>
2012-07-19 03:44:46 -0400

    Add text as a property of org_heading

--- src/org_heading.h
+++ src/org_heading.h
@@ -5,6 +5,10 @@
 #ifndef ORG_HEADING_H
 #define ORG_HEADING_H
 
+/* org_text forward declaration */
+struct org_text;
+typedef struct org_text org_text;
+
 /* org_heading forward declaration */
 struct org_heading;
 typedef struct org_heading org_heading;
@@ -18,5 +22,9 @@ int   org_heading_get_level (org_heading *self);
 void  org_heading_set_level (org_heading *self, int level);
 char *org_heading_get_text (org_heading *self);
 void  org_heading_set_text (org_heading *self, char *heading_text);
+void org_heading_set_have_property_list (org_heading *h, bool have_one);
+void org_heading_add_property_empty (char *string);
+org_text *org_heading_get_text_elt (org_heading *self);
+void org_heading_set_text_elt (org_heading *self, org_text * text);
 
 #endif
