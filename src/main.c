3b98aa08d4ee886ef065b9dd2d8b2b94266ca38e
Andrew Young <youngar17@gmail.com>
2012-07-27 09:09:38 -0400

    Update main to use new elements

--- src/main.c
+++ src/main.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <iconv.h>
+#include "debug.h"
 
 #include "doc_stream.h"
+#include "doc_ref.h"
+#include "print.h"
+#include "org_document.h"
+#include "doc_elt.h"
+#include "doc_elt_ops.h"
 #include "org_parser.h"
-#include "merge_tree.h"
-#include "doc_tree.h"
-#include "merge_print.h"
-#include "doc_merge.h"
-#include "merge_print_ctxt.h"
 
 int
 main (int argc, char *argv[])
@@ -17,10 +18,10 @@ main (int argc, char *argv[])
   /* Process command line arguments */
   char *progname = argv[0];
   FILE * out = NULL;
-
+  debug_msg (MAIN, 4, "Staring program\n");
   if (argc < 4)
     {
-      printf ("%s: missing operands", argv[0]);
+      printf ("%s: missing operands\n", argv[0]);
     }
   if (argc == 5)
     {
@@ -28,30 +29,55 @@ main (int argc, char *argv[])
     }
   if (out == NULL)
     {
+      debug_msg (MAIN, 4, "Setting output to stdout\n");
       out = stdout;
     }
 
   FILE *anc_file = fopen ( argv[1], "r");
   if (anc_file != NULL)
     {
+      debug_msg (MAIN, 4, "File 1 opened\n");
       FILE *loc_file = fopen (argv[2], "r");
       if (loc_file != NULL)
 	{
+	  debug_msg (MAIN, 4, "File 2 opened\n");
 	  FILE *rem_file = fopen (argv[3], "r");
 	  if (rem_file != NULL)
 	    {
-	      doc_tree *anc_dtree = org_parse_file_stream (anc_file);
-	      doc_tree *loc_dtree = org_parse_file_stream (loc_file);
-	      doc_tree *rem_dtree = org_parse_file_stream (rem_file);
+	      debug_msg (MAIN, 4, "File 3 opened\n");
+	      debug_msg (MAIN, 4, "Parsing Files\n\n");
+	      org_document *anc = org_parse_file_stream (anc_file, ANC_SRC);
+	      org_document *loc = org_parse_file_stream (loc_file, LOC_SRC);
+	      org_document *rem = org_parse_file_stream (rem_file, REM_SRC);
+#if MAIN_PRINTLEVEL >= 4
+	      struct print_ctxt dbgctxt;
+	      print_ctxt_init (&dbgctxt);
+	      debug_msg (MAIN, 3, "Printing Ancestor\n");
+	      org_document_print (anc, &dbgctxt, stderr);
+	      debug_msg (MAIN, 3, "Printing Local\n");
+	      org_document_print (loc, &dbgctxt, stderr);
+	      debug_msg (MAIN, 3, "Printing Remote\n");
+	      org_document_print (rem, &dbgctxt, stderr);
+#endif
+	      debug_msg (MAIN, 4, "Merging Files\n\n");
+	      debug_msg (MAIN, 4, "Merging anc and loc\n");
+	      org_document_merge (anc, loc);
+	      debug_msg (MAIN, 4, "Merging anc and rem\n");
+	      org_document_merge (anc, rem);
 
-	      merge_tree *mtree = create_merge_tree(anc_dtree, loc_dtree, rem_dtree);
+	      struct print_ctxt ctxt;
+	      print_ctxt_init (&ctxt);
+	      debug_msg (MAIN, 3, "Printing Ancestor\n\n");
+	      org_document_print (anc, &ctxt, stdout);
 
+	      /*
+	      debug_msg (MAIN, 4, "Printing Files\n\n");
 	      merge_print_ctxt mp_ctxt;
 	      mp_ctxt.depth = 0;
 	      mp_ctxt.content_conflict_state = 0;
 	      mp_ctxt.structural_conflict_state=0;
 	      merge_print (mtree, &mp_ctxt, out);
-
+	      */
 	      fclose (rem_file);
 	    }
 	  fclose (loc_file);
@@ -64,7 +90,7 @@ main (int argc, char *argv[])
     {
       fclose (out);
     }
-
+  debug_msg (MAIN, 4, "Closing program\n");
   /* Exit */
   return EXIT_SUCCESS;
 }
